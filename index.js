b8c59a672ed80ea0a80cccbb0866346bfa385200
AlaminMJ <86642366+AlaminMJ@users.noreply.github.com>
2023-02-28 17:17:01 +0600

    2 files modified

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import express from 'express';
-
+import http from 'http';
+import { Socket } from 'socket.io';
 const app = express();
+const server = http.createServer(app);
+server.listen(3000, () => console.log('Server is ok'));
+const io = new Socket(server);
+3;
+io.on('connection', (socket) => {
+  console.log(socket.id);
+});
